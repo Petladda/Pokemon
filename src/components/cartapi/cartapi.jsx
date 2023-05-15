8cc35e0b8941c2aa9b715e2a9268114a2189d225
Helio Trope <petladda.94@gmail.com>
2023-05-15 18:04:48 +0700

    fix: cart pokemon

--- src/components/cartapi/cartapi.jsx
+++ src/components/cartapi/cartapi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect } from "react";
 import { useState } from "react";
-import { Await } from "react-router-dom";
+
 
 export const Cartapi = () =>{
 
@@ -11,8 +11,6 @@ export const Cartapi = () =>{
        const url = 'https://pokeapi.co/api/v2/pokemon?limit=100';
         const response = await axios.get(url)
          pokemonData(response.data.results)
-         
-       
  
     }
 
@@ -20,23 +18,40 @@ export const Cartapi = () =>{
         response.map(async(item)=>{
             const result = await axios.get(item.url);
             const object = {
+                id: result.data.order,
                 name: result.data.name,
                 image: result.data.sprites.front_default
             }
             setPokemon(prev => [...prev,object])
           
+            console.log("setPokemon",result)
         })
     }
-    console.log("setPokemon",pokemon)
 
     useEffect(()=>{
         pokemonfetch()
     },[])
 
 
-    return(
-        <div>
-            heloo
-        </div>
+    return (
+       <div className="grid grid-cols-4 gap-4 px-6 cursor-pointer mt-5">
+            {pokemon.map((item)=>{
+                return(
+                    <div className="flex border-2 border-amber-200 shadow-md rounded-xl drop-shadow-xl justify-around mb-2 bg-slate-500 hover:bg-amber-500 hover:text-white  " key={item.id}>
+                        <div className=" w-32 my-8  text-center  md:text-sm lg:text-xl  ">
+                            <h5 >#{item.id}</h5>
+                            <h5 className="text-white mb-1">{item.name}</h5>
+                            <button className="text-xs ml-2 w-16 bg-amber-400 rounded-2xl hover:bg-slate-700 ">details..</button>
+                        </div>
+                        <div className="h-full w-30 mb-4 mt-4 items-end  hover:scale-125 md: ">
+                        <img  src={item.image}></img>
+                        </div>
+                        
+                    </div>
+                )
+                    
+                
+            })}
+       </div>
     )
 }
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -8,23 +8,23 @@ export const Form = ()=>{
    
     
     return (
-        <div>
+        <div className="">
             <button>
-            <Link to="/" className="text-amber-500"> HOME</Link>
+            <Link to="/" className=" text-amber-600"> HOME</Link>
             </button>
-            <form className="flex flex-col justify-center items-center border border-amber-600 m-auto w-80 h-56 text-center mt-2" 
+            <form className="flex flex-col justify-center items-center border border-amber-600 m-auto w-80 h-56 text-center mt-2 " 
                 onSubmit={handleSubmit((data)=>{
                 console.log(data)
                 
             })}>
-                <label htmlFor="username" className="mb-2 ">Username</label>
-                <input className="h-5 border " {...register("username",{required : "Username this is required"})}
+                <label htmlFor="username" className="mb-2 text-white">Username</label>
+                <input className="h-6 border " {...register("username",{required : "Username this is required"})}
                 aria-invalid={errors.username ? "true": "false"} placeholder="username" />
 
                 {errors.username && <p className="text-red-600" role="alert">{errors.username?.message}</p>}
 
                 <label className="mb-2" htmlFor="email">Email</label>
-                <input className="h-5 border" {...register("email",{required : "this is required"})} placeholder="email"/>
+                <input className="h-6 border" {...register("email",{required : "this is required"})} placeholder="email"/>
 
                 <button type="submit" className="text-emerald-500 hover:text-amber-600/70 mt-3">Submit</button>
             </form>
